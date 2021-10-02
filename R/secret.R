018d14fed5576b7d94a154f377f130423587f2c5
aftonsteps <aftoncoombs@gmail.com>
2021-10-01 20:09:52 -0700

    Initial commit

---
+++ R/secret.R
@@ -0,0 +1,6 @@
+pick_a_hat <- function() {
+  hats <- c("mushroom", "newsboy", "sparkle headband", "cat ears 1",
+            "cat ears 2", "bunny ears", "cat ears 3", "purple wig")
+
+  return(sample(hats, 1))
+}
