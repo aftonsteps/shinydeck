9cf2ea40f1944380ff280851ec253bb91a643905
aftoncoombs <aftoncoombs@gmail.com>
2021-10-07 23:34:05 -0700

    Housekeeping
    - Removed files not needed
    - Added rsconnect file and dependencies

--- R/secret.R
+++
@@ -1,7 +0,0 @@
-pick_a_hat <- function() {
-  hats <- c("mushroom", "newsboy", "sparkle headband", "cat ears 1",
-            "cat ears 2", "bunny ears", "purple wig",
-            "butterfly", "tiara", "birthday hat")
-
-  return(sample(hats, 1))
-}
